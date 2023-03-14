ae1461567c54a3d2f060562e317d29841cbddc0a
Rohit <rohit@gloify.com>
2023-03-15 02:17:09 +0530

    Initial commit

---
+++ src/AnimalShow.js
@@ -0,0 +1,38 @@
+import "./AnimalShow.css";
+import { useState } from "react";
+import bird from "./bird.svg";
+import cat from "./cat.svg";
+import cow from "./cow.svg";
+import dog from "./dog.svg";
+import gator from "./gator.svg";
+import heart from "./heart.svg";
+import horse from "./horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+function AnimalShow({ type }) {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+  return (
+    <div className="animal-show" onClick={handleClick}>
+      <img className="animal" alt="animal" src={svgMap[type]} />
+      <img
+        className="heart"
+        alt="heart"
+        src={heart}
+        style={{ width: 10 + 10 * clicks + "px" }}
+      />
+    </div>
+  );
+}
+export default AnimalShow;
